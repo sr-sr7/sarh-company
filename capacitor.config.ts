61177dffa1e988452465888ca24601493ae78320
abdulmajeed <asimjed2@gmail.com>
2026-06-03 08:03:22 +0000

    Add Capacitor config for iOS app

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sarh.company',
+  appName: 'صرح',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
